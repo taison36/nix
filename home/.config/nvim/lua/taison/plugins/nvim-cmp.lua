e4192a2ee7e3082a5f2101e12b3d34ae6d7a3a5a
taison36 <tihon.korol77@gmail.com>
2026-07-18 12:20:03 +0200

    nvim: tab for accepting the next suggestion

--- home/.config/nvim/lua/taison/plugins/nvim-cmp.lua
+++ home/.config/nvim/lua/taison/plugins/nvim-cmp.lua
@@ -39,6 +39,13 @@ return {
         ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.confirm({ select = true }) -- accept next suggestion
+          else
+            fallback() -- otherwise insert a normal tab
+          end
+        end, { "i", "s" }),
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
